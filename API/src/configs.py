8f22d90c290cba569eb810eadf80676f39c1dbc3
Duy Nguyen (Intern) <intern05@dekvn.internal>
2019-10-16 09:20:39 +0700

    move api to other repo

--- API/src/configs.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-#mongoDB connection string
-mongodb = 'mongodb+srv://libary:ecyfAnTIz058VdrP@cluster0-uuoo5.gcp.mongodb.net/test?retryWrites=true&w=majority'
-client = MongoClient(mongodb)
-db = client.library
-
-#token
-tokenExpireTime = 300 #seconds
