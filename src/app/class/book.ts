3dead16b003ddc9648463e4f95ee0b116b0b5808
Duy <zzxxcc2056@gmail.com>
2019-10-21 20:53:59 +0700

    add child-book class

--- src/app/class/book.ts
+++ src/app/class/book.ts
@@ -1,13 +1,15 @@
+import { ChildBook } from './child-book';
+
 export class Book {
   public isbn: string;
   public name: string;
-  public price: number;
   public author: string;
+  public books: Array<ChildBook>;
 
-  constructor(isbn?: string, name?: string, price?: number, author?: string){
+  constructor(isbn?: string, name?: string, author?: string, books?: Array<ChildBook>){
     this.isbn = isbn || '';
     this.name = name || '';
-    this.price = price || 0;
     this.author = author || '';
+    this.books = books || new Array<ChildBook>();
   }
 }
