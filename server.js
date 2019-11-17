8d3dfa5a5d371baed35b59c5c8f1bedc46c2ab8a
Duy <zzxxcc2056@gmail.com>
2019-11-17 20:19:43 +0700

    try fix 304 http 3

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.disable('etag');
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist/Angular'));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/Angular/index.html'));
 });
-app.disable('etag');
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
