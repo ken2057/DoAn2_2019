40b619c7020f4ccf34dc9457a5898975996dc032
Duy <zzxxcc2056@gmail.com>
2019-11-17 20:03:08 +0700

    try fix 304 http

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(express.static(__dirname + '/dist/Angular'));
 app.use(bodyParser.json());
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname + '/dist/Angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Angular/index.html'));
 });
+app.disable('etag');
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
