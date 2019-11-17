39cde65781dbb8f9a9138a5ed219b07e8be9cbc5
duy <zzxxcc2056@gmail.com>
2019-11-17 21:46:36 +0700

    try fix 304 7

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ const app = express();
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/<name-of-app>'));
 app.disable('etag');
-app.use(bodyParser.json());
+app.use(bodyParser.json(), { maxAge: 2592000000 });
 app.use(express.static(__dirname + '/dist/Angular'));
 
-// app.get('/*', function(req,res) {
-//    res.sendFile(path.join(__dirname + '/dist/Angular/index.html'));
-// });
+
+app.get('/*', function(req,res) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    res.sendFile(path.join(__dirname + '/dist/Angular/index.html'));
+    next()
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
